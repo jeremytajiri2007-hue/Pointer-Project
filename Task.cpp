28c9dcb3e6e275218e3b7c54426e4d94de6810e1
jeremytajiri2007-hue <jeremytajiri2007@gmail.com>
2025-12-08 21:23:57 -0800

    Update Task.cpp

--- Task.cpp
+++ Task.cpp
@@ -1,1 +1,8 @@
+```cpp
+
+#include "Task.h"
+
+// No additional implementation needed as everything is inlined
+// If you want, you could move methods here, but inline is simpler for this project.
+
 
