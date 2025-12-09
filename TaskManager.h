64bea14fbe3922a7eb776716aa4ddd27a82c7254
jeremytajiri2007-hue <jeremytajiri2007@gmail.com>
2025-12-08 21:23:08 -0800

    Update TaskManager.h

--- TaskManager.h
+++ TaskManager.h
@@ -1,1 +1,25 @@
+```cpp
+
+#ifndef TASKMANAGER_H
+#define TASKMANAGER_H
+
+#include "Task.h"
+#include <memory>
+#include <iostream>
+
+class TaskManager {
+private:
+    std::unique_ptr<Task[]> tasks;
+    int size;
+    int capacity;
+
+public:
+    TaskManager(int initialCapacity);
+
+    void addTask(const std::string& desc);
+    void removeTask(int id);
+    void listTasks() const;
+};
+
+#endif
 
